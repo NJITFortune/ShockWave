f822ab96c95403d152e2f921a18dfb9428599431
Eric Fortune <eric.fortune@njit.edu>
2026-01-25 15:16:09 -0500

    Mapping out the grip and setting up the analysis.

---
+++ resources/coordinates-LONG.m
@@ -0,0 +1,26 @@
+ydist = 50 * sqrt(3) / 2;
+
+XY = [
+    % Bottom row
+     25   0
+     75   0
+    125   0
+    175   0
+    225   0
+
+    % Middle row
+      0   ydist
+     50   ydist
+    100   ydist
+    150   ydist
+    200   ydist
+    250   ydist
+
+    % Top row
+     25   2*ydist
+     75   2*ydist
+    125   2*ydist
+    175   2*ydist
+    225   2*ydist
+];
+
